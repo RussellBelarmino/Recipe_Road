ad76d7408649498fe31117abc3d5884a5150d1d1
RussellBelarmino <russellbelarmino8@gmail.com>
2022-07-29 20:48:54 -0400

    Re-added a new page on the storyboard, but this time there's a class for the new view controller

---
+++ Recipe_Road/InputtingIngredientsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InputtingIngredientsViewController.swift
+//  Recipe_Road
+//
+//  Created by CoopStudent on 2022-07-29.
+//
+
+import UIKit
+
+class InputtingIngredientsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
